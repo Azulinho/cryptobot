d5cb2563de693afbab4b1763ac01775f72f90038
Azulinho <mail@azulinho.com>
2022-11-07 12:35:10 +0000

    make

---
+++ klines_caching_service/tests/test_klines_caching_service.py
@@ -0,0 +1,12 @@
+""" pytests tests for app.py """
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=redefined-outer-name
+# pylint: disable=import-outside-toplevel
+# pylint: disable=no-self-use
+
+
+class TestKlinesCaching_service:
+    def test_placeholder(self):
+        pass
