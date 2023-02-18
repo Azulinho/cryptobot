dd581924751240612fb6be825eb40eb47b7aaf21
Azulinho <mail@azulinho.com>
2023-02-18 20:33:58 +0000

    refactors prove-backtesting/automated-backtesting
    Merged the automated-backtesting into the prove-backtesting script and
    rewrote it from scratch. It now consumes price log files from the price
    log server, reducing IO and the extending the life of my SSDs.
    
    There is additional work to be done on prove-backtesting and the
    price-log-service. Where currently I pull an index.json.gz and retrieve
    the list of individual coin files from the price log service, I need to
    change this so that the the index.json.gz only contains the list of
    coins available on a single price daily log file.
    The prove-backtesting will retrieve that index, and ask for the
    individual date log alongside the coins it wants to retrieve from that
    log.
    The price log service will be responsible for filtering the klines
    from the main daily price logs instead of relying on indivual coin based
    price logs.
    Finally the download klines script will have to be modified so not write
    the individual coin files, but only the index.json.gz
    With these changes we will reduce considerably the amount of disk
    required for backtesting. From 160GB to 8GB

--- utils/automated-backtesting.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-ulimit -n 65535
-source /cryptobot/.venv/bin/activate
-python -u utils/automated-backtesting.py \
-	-l ${LOGFILE} -c ${CONFIG_FILE} -m ${MIN} -f "${FILTER}" -s "${SORTBY}"
